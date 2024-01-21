707ebaaa189d4f86ea7ee604d1d2343b733f1d71
yanis <yanismehalaine@outlook.fr>
2024-01-21 20:13:31 +0100

    feature(Header): imported Header in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
+
+console.log(Header);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
   </React.StrictMode>
 );

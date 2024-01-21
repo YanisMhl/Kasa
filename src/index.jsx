00325b0fe25c479cc442382642c89d5fc63f5242
yanis <yanismehalaine@outlook.fr>
2024-01-21 20:26:58 +0100

    feat(home/index): imported Home to index and added routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from './components/Header';
+import Home from './pages/Home';
 
 console.log(Header);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );

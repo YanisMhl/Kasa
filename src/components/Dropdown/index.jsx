142181a2bca8bd21fa13b37d821f8b04e6d172c6
yanis <yanismehalaine@outlook.fr>
2024-01-23 05:55:50 +0100

    feat(Dropdown): open animation when user clicks on dropdown icon

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,14 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-function Dropdown({ title, content }) {
+function Dropdown({ title, content, size }) {
+    const [open, setOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setOpen(!open);
+    }
+
     return (
         <div className="dropdown">
             <div className="dropdown-title">
-                <h2>{title}</h2>
-                <FontAwesomeIcon icon={faChevronDown} />
+                <p>{title}</p>
+                <FontAwesomeIcon icon={faChevronUp} className={`dropdown-icon ${open ? 'rotate' : ''}`} size='xl' onClick={toggleDropdown} />
             </div>
-            <div className="dropdown-content">
+            <div className="dropdown-content" style={{display: open ? 'block' : 'none' }}>
                 {content.map((item, index) => (
                     <p key={`dropdown-item-${index}`}
                     className="dropdown-item">{item}</p>

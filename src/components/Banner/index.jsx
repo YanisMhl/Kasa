5a3ab131850aeb5eedefa97c9d403a28e32dcd46
yanis <yanismehalaine@outlook.fr>
2024-01-26 13:10:47 +0100

    refactor(Banner): better styling for banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,7 @@
 function Banner({ children, background }) {
     return (
         <div className="banner" style={{
-            background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${background})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center center',
-            }}>
+            background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${background})`}}>
             {children}
         </div>
     );

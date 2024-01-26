8105a4b294dbfbc083e49f2118c2456a6f1935e3
yanis <yanismehalaine@outlook.fr>
2024-01-26 12:49:11 +0100

    created tag component

---
+++ src/components/Tag/index.jsx
@@ -0,0 +1,7 @@
+function Tag({ title }) {
+    return (
+        <p className="tag">{title}</p>
+    );
+}
+
+export default Tag;
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import Carousel from "../../components/Carousel";
 import Dropdown from "../../components/Dropdown";
+import Tag from "../../components/Tag";
 import '../../css/logement.css';
 import { getSingleLogement } from "../../services/get_data";
 import { useEffect, useState } from "react";
@@ -16,6 +17,12 @@ function Logement() {
         getSingleLogement(logementId).then((data) => setLogementInfo(data));
     }, []);
 
+    if (!logementInfo.host) {
+        return (
+            <h1>Chargement..</h1>
+        );
+    }
+
     return (
         <div className="logement">
             <Carousel id={logementId}/>
@@ -26,27 +33,25 @@ function Logement() {
                         <h2>{logementInfo.location}</h2>
                     </div>
                     <div className="host">
-                        <p className="host-name">{logementInfo.host ? logementInfo.host.name : ''}</p>
-                        <img className="host-picture" src={logementInfo.host ? logementInfo.host.picture : ''} alt="host picture"/>
+                        <p className="host-name">{logementInfo.host.name}</p>
+                        <img className="host-picture" src={logementInfo.host.picture} alt="host picture"/>
                     </div>
                 </div>
                 <div className="info-row-2">
                     <div className="tags">
-                        {logementInfo.tags ? logementInfo.tags.map((tag, index) => (
-                            <div className="tag" key={index}>
-                                <p>{tag}</p>
-                            </div>
-                        )) : ''}
+                        {logementInfo.tags.map((tag, index) => (
+                            <Tag title={tag} key={index} />
+                        ))}
                     </div>
                     <div className="ratings">
-                        {logementInfo.rating ? Array(5).fill().map((_, index) => (
+                        {Array(5).fill().map((_, index) => (
                             <FontAwesomeIcon key={index} size="2x" icon={index + 1 <= parseInt(logementInfo.rating) ? solidStar : regularStar} />
-                        )) : ''}
+                        ))}
                     </div>
                 </div>
                 <div className="dropdowns">
-                    <Dropdown title="Description" content={logementInfo.description ? [logementInfo.description] : []} />
-                    <Dropdown title="Equipements" content={logementInfo.equipments ? logementInfo.equipments : []} />
+                    <Dropdown title="Description" content={[logementInfo.description]} />
+                    <Dropdown title="Equipements" content={logementInfo.equipments} />
                 </div>
             </div>
         </div>

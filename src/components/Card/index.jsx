7d3102b43c5817ef02e637ba5964a65b7447463e
yanis <yanismehalaine@outlook.fr>
2024-02-20 16:21:11 +0100

    fix(css): fixed card cover

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,9 @@ function Card({ id, title, cover }) {
     return (
         <Link to={`/logement/${id}`} className="card" style={{
             background: `linear-gradient(to bottom, rgba(255, 255, 255, 0),
-            rgba(0, 0, 0, 1)), url(${cover})`}}>
+            rgba(0, 0, 0, 1)), url(${cover})`,
+            backgroundSize: "cover"
+            }}>
             <p>{title}</p>
         </Link>
     );

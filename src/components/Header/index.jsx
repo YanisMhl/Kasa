7db2788075f923c1e9b065265d47b43676ebd190
yanis <yanismehalaine@outlook.fr>
2024-02-26 18:56:56 +0100

    refactor(Header): use NavLink instead of Link to underline the active Link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,14 @@
 import logo from '../../assets/logo.png';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 function Header() {
-    const location = useLocation();
     return (
         <div className="header">
             <img src={logo} alt="Logo Kasa"/>
             <nav>
-                <Link to="/" className={`${location.pathname === '/' ? 'underline' : ''}`}>Accueil</Link>
-                <Link to="/a-propos" className={`${location.pathname === '/a-propos' ? 'underline' : ''}`}>A Propos</Link>
+                <NavLink to="/">Accueil</NavLink>
+                <NavLink to="/a-propos">A propos</NavLink>
             </nav>
         </div>
     )

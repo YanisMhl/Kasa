ffa0a6804d11621b13c6d2d02989985e6ac79b28
yanis <yanismehalaine@outlook.fr>
2024-02-28 09:23:27 +0100

    fix(wsl): wsl2doesn't support hot reload on windows hard drive, downgraded react-scripts version and node version to make it work

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -7,7 +7,6 @@ import { getSingleLogement } from "../../services/get_data";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
 function Logement() {
     const { logementId } = useParams();
@@ -33,22 +32,22 @@ function Logement() {
         <div className="logement">
             <Carousel pictures={logementInfo.pictures}/>
             <div className="info">
-                <div className="info-row-1">
+                <div className="info-column-1">
                     <div className="title">
                         <h1>{logementInfo.title}</h1>
                         <h2>{logementInfo.location}</h2>
                     </div>
-                    <div className="host">
-                        <p className="host-name">{logementInfo.host.name}</p>
-                        <img className="host-picture" src={logementInfo.host.picture} alt="host picture"/>
-                    </div>
-                </div>
-                <div className="info-row-2">
                     <ul className="tags">
                         {logementInfo.tags.map((tag, index) => (
                             <Tag title={tag} key={index} />
                         ))}
                     </ul>
+                </div>
+                <div className="info-column-2">
+                <div className="host">
+                        <p className="host-name">{logementInfo.host.name}</p>
+                        <img className="host-picture" src={logementInfo.host.picture} alt="host picture"/>
+                    </div>
                     <div className="ratings">
                         {Array(5).fill().map((_, index) => (
                             <FontAwesomeIcon key={index} size="2x" icon={solidStar} className={index + 1 <= parseInt(logementInfo.rating) ? "solid-star" : "regular-star"} />
